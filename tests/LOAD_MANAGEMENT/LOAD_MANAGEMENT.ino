acadab30ea38b8ea08eb0a59ec991ad4448e6ffb
Vince <marc.bentoy@gmail.com>
2023-02-05 11:32:47 +0800

    fix: lcd size

--- tests/LOAD_MANAGEMENT/LOAD_MANAGEMENT.ino
+++ tests/LOAD_MANAGEMENT/LOAD_MANAGEMENT.ino
@@ -2,7 +2,7 @@
 #include "EmonLib.h"
 #include <Wire.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 20, 4); 
 
 // CURRENT SENSOR OBJECTS
 EnergyMonitor eMon1; // Create an instance</p>
