ec213bfbae804b779d4ea5c7f419d24810e5119b
Vince <marc.bentoy@gmail.com>
2023-04-02 04:11:17 +0800

    :sparkles: add new display

--- src/Main/Main.ino
+++ src/Main/Main.ino
@@ -56,6 +56,21 @@ PZEM004Tv30 pzem;
 bool isTesting = true;
 int testing_battery_capacity = 100;
 
+/* LCD CUSTOM CHARACTERS */
+byte customBattFull[] = { 0x0E, 0x0A, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F };
+byte customBatt80AndUp[] = { 0x0E, 0x0A, 0x11, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F };
+byte customBatt60AndUp[] = { 0x0E, 0x0A, 0x11, 0x11, 0x1F, 0x1F, 0x1F, 0x1F };
+byte customBatt50AndBelow[] = { 0x0E, 0x0A, 0x15, 0x15, 0x11, 0x15, 0x11, 0x1F };
+byte customLightning[] = { 0x00, 0x02, 0x04, 0x0C, 0x1F, 0x06, 0x04, 0x08 };
+byte customPlug[] = { 0x0A, 0x0A, 0x1F, 0x11, 0x11, 0x0E, 0x04, 0x04 };
+byte customLow[] = { 0x0E, 0x1F, 0x1B, 0x1B, 0x0A, 0x11, 0x1B, 0x0E };
+byte customWarning[] = { 0x0E, 0x11, 0x15, 0x15, 0x11, 0x15, 0x11, 0x0E };
+
+/* BOOLEANS */
+int lowLoadStatus = 0;
+int midLoadStatus = 0;
+int highLoadStatus = 0;
+
 void setup() {
     Serial.begin(9600);
 
@@ -85,6 +100,17 @@ void setup() {
     lcd.init();
     lcd.backlight();
 
+    // LCD create custom characters
+    lcd.createChar(0, customBattFull);
+    lcd.createChar(1, customBattFull);
+    lcd.createChar(2, customBatt80AndUp);
+    lcd.createChar(3, customBatt60AndUp);
+    lcd.createChar(4, customBatt50AndBelow);
+    lcd.createChar(5, customLightning);
+    lcd.createChar(6, customPlug);
+    lcd.createChar(7, customLow);
+    lcd.createChar(8, customWarning);
+
     // initial bootup functions 
     readSRNE();
     allowLoadConnection();
@@ -104,7 +130,9 @@ void loop() {
     // trip off load connection based on load power and present battery level
     tripOffLoadConnection();
 
-    // TODO: add lcd display
+    // TODO: add lcd readings and status display
+    displayData();
+
 
     // delay
     delay(POWER_READING_INTERVAL * 1000);
@@ -157,9 +185,18 @@ void readSRNE() {
 }
 
 void allowLoadConnection() {
-    if (srne_battery_capacity >= LOW_LOAD_BATT_REQ) digitalWrite(LOW_RELAY_PIN, LOW); // allows low load connection
-    if (srne_battery_capacity >= MID_LOAD_BATT_REQ) digitalWrite(MID_RELAY_PIN, LOW); // allows mid load connection
-    if (srne_battery_capacity >= HIGH_LOAD_BATT_REQ) digitalWrite(HIGH_RELAY_PIN, LOW); // allows high load connection
+    if (srne_battery_capacity >= LOW_LOAD_BATT_REQ) {
+        digitalWrite(LOW_RELAY_PIN, LOW); // allows low load connection
+        lowLoadStatus = 2;
+    }
+    if (srne_battery_capacity >= MID_LOAD_BATT_REQ) {
+        digitalWrite(MID_RELAY_PIN, LOW); // allows mid load connection
+        midLoadStatus = 2;
+    }
+    if (srne_battery_capacity >= HIGH_LOAD_BATT_REQ) {
+        digitalWrite(HIGH_RELAY_PIN, LOW); // allows high load connection
+        highLoadStatus = 2;
+    }
 }
 
 void readPZEM() {
@@ -173,12 +210,132 @@ void readPZEM() {
 
 void tripOffLoadConnection() {
     // trips off load connection based on battery level
-    if (srne_battery_capacity < HIGH_LOAD_BATT_REQ) digitalWrite(HIGH_RELAY_PIN, HIGH); // allows high load connection
-    if (srne_battery_capacity < MID_LOAD_BATT_REQ) digitalWrite(MID_RELAY_PIN, HIGH); // allows mid load connection
-    if (srne_battery_capacity < LOW_LOAD_BATT_REQ) digitalWrite(LOW_RELAY_PIN, HIGH); // allows low load connection
+    if (srne_battery_capacity < HIGH_LOAD_BATT_REQ) {
+        digitalWrite(HIGH_RELAY_PIN, HIGH); // trips off high load connection
+        highLoadStatus = 1;
+    }
+    if (srne_battery_capacity < MID_LOAD_BATT_REQ) {
+        digitalWrite(MID_RELAY_PIN, HIGH); // trips mid load connection
+        midLoadStatus = 1;
+    }
+    if (srne_battery_capacity < LOW_LOAD_BATT_REQ) {
+        digitalWrite(LOW_RELAY_PIN, HIGH); // allows low load connection
+        lowLoadStatus = 1;
+    }
 
     // trips off load connection based on load power 
-    if (power >= LOW_LOAD_MAX_POWER) digitalWrite(LOW_RELAY_PIN, HIGH);     // trips off low load connection
-    if (power >= MID_LOAD_MAX_POWER) digitalWrite(MID_RELAY_PIN, HIGH);     // trips off mid load connection
-    if (power >= HIGH_LOAD_MAX_POWER) digitalWrite(HIGH_RELAY_PIN, HIGH);   // trips off high load connection
+    if (power > LOW_LOAD_MAX_POWER) {
+        digitalWrite(LOW_RELAY_PIN, HIGH);     // trips off low load connection
+        lowLoadStatus = 0;
+    }
+    if (power > MID_LOAD_MAX_POWER) {
+        digitalWrite(MID_RELAY_PIN, HIGH);     // trips off mid load connection
+        midLoadStatus = 0;
+    }
+    if (power > HIGH_LOAD_MAX_POWER) {
+        digitalWrite(HIGH_RELAY_PIN, HIGH);   // trips off high load connection
+        highLoadStatus = 0;
+    }
+}
+
+void displayData() {
+    lcd.clear();
+
+    /******** ROW 0  **********/
+    // * BATTERY LEVEL
+    lcd.setCursor(0, 1);
+    if (srne_battery_capacity == 100) lcd.write(0); // **
+    if (srne_battery_capacity >= 80) lcd.write(1);  // battery 
+    if (srne_battery_capacity >= 60) lcd.write(2);  // symbol
+    if (srne_battery_capacity <= 50) lcd.write(3);  // **
+
+    lcd.setCursor(0, 3);                // **
+    lcd.print(srne_battery_capacity);   // battery level value
+    lcd.print(" % | ");                 // **
+
+    lcd.setCursor(0, 11);   // **
+    lcd.write(4);           // lightning symbol
+    lcd.setCursor(0, 13);   // ** 
+    lcd.print(power);       // power
+    lcd.setCursor(0, 18);   // value
+    lcd.print("W");         // **
+    /************************/
+
+    /******** ROW 1  **********/
+    lcd.setCursor(1, 0);
+    lcd.print("LOW: ");
+    if (lowLoadStatus == 0) {
+        lcd.setCursor(1, 6);
+        lcd.print("OVERLOADED  ");
+        lcd.write(6);
+    }
+    if (lowLoadStatus == 1) {
+        lcd.setCursor(1, 5);
+        lcd.print("INSUFFICIENT ");
+        lcd.write(7);
+    }
+    if (lowLoadStatus == 2) {
+        lcd.setCursor(1, 6);
+        lcd.print("CONNECTED   ");
+        lcd.write(5);
+    }
+    /************************/
+
+    /******** ROW 2  **********/
+    lcd.setCursor(2, 0);
+    lcd.print("MID: ");
+    if (midLoadStatus == 0) {
+        lcd.setCursor(2, 6);
+        lcd.print("OVERLOADED  ");
+        lcd.write(6);
+    }
+    if (midLoadStatus == 1) {
+        lcd.setCursor(2, 5);
+        lcd.print("INSUFFICIENT ");
+        lcd.write(7);
+    }
+    if (midLoadStatus == 2) {
+        lcd.setCursor(2, 6);
+        lcd.print("CONNECTED   ");
+        lcd.write(5);
+    }
+    /************************/
+
+    /******** ROW 3  **********/
+    lcd.setCursor(3, 0);
+    lcd.print("HIG: ");
+    if (highLoadStatus == 0) {
+        lcd.setCursor(3, 6);
+        lcd.print("OVERLOADED  ");
+        lcd.write(6);
+    }
+    if (highLoadStatus == 1) {
+        lcd.setCursor(3, 5);
+        lcd.print("INSUFFICIENT ");
+        lcd.write(7);
+    }
+    if (highLoadStatus == 2) {
+        lcd.setCursor(3, 6);
+        lcd.print("CONNECTED   ");
+        lcd.write(5);
+    }
+    /************************/
+
+    /* SERIAL PRINT */
+    Serial.print("Battery Level: ");
+    Serial.print(srne_battery_capacity);
+    Serial.print("% | ");
+    Serial.print("Load Power: ");
+    Serial.print(power);
+    Serial.println("W");
+
+    Serial.print("LOW Load Status: ");
+    Serial.println(lowLoadStatus);
+    Serial.print("MID Load Status: ");
+    Serial.println(midLoadStatus);
+    Serial.print("HIGH Load Status: ");
+    Serial.println(highLoadStatus);
+
+    Serial.println("- - - - - - - - - - -");    // separator
+    /***************/
 }
